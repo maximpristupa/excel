24decb5ffc5c3d12413ab113ffff774a6ab41e0a
maximpristupa <g.maxim.pristupa@gmail.com>
2019-10-21 15:15:43 +0300

    read and write file

---
+++ index.js
@@ -0,0 +1,56 @@
+// Data
+
+const fs = require('fs');
+const row_data = [
+  {
+    name: 'Ivan',
+    surname: 'Ivanov',
+    age: 20,
+    gender: 'M'
+  }, {
+    name: 'Fedor',
+    surname: 'Fedorov',
+    age: 25,
+    gender: 'M',
+  }, {
+    name: 'Maria',
+    surname: 'Brown',
+    age: 78,
+    gender: 'F'
+  }, {
+    name: 'Maria',
+    surname: 'Brown',
+    age: 78,
+    gender: 'F'
+  }
+];
+
+// Write
+
+function write_csv(data, file = 'data.csv') {
+	const fastcsv = require('fast-csv');
+	// const fs = require('fs');
+	const ws = fs.createWriteStream(file);
+	fastcsv
+	  .write(data, { headers: true })
+	  .pipe(ws);
+	console.log('The CSV file was written successfully');
+}
+
+// Read
+
+function read_csv(file) {
+	const csv = require('csv-parser');
+	// const fs = require('fs');
+	fs.createReadStream(file)
+	  .pipe(csv())
+	  .on('data', (row) => {
+	    console.log(row);
+	  })
+	  .on('end', () => {
+	    console.log('CSV file successfully processed');
+	  });
+}
+
+write_csv(row_data)
+read_csv("data.csv")
