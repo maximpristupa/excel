d82fb3fe616bdab6826d6520fcb6a345c56b9c7f
maximpristupa <g.maxim.pristupa@gmail.com>
2019-10-23 16:17:19 +0300

    implementation of the requested changes

--- evaluate.js
+++ evaluate.js
@@ -1,78 +1,88 @@
-Array.prototype.clean = function() {
-    for(let i = 0; i < this.length; i++) {
-        if(this[i] === "") {
-            this.splice(i, 1);
-        }
+const operators = {
+    "^": {
+        precedence: 4,
+        associativity: "Right"
+    },
+    "/": {
+        precedence: 3,
+        associativity: "Left"
+    },
+    "*": {
+        precedence: 3,
+        associativity: "Left"
+    },
+    "+": {
+        precedence: 2,
+        associativity: "Left"
+    },
+    "-": {
+        precedence: 2,
+        associativity: "Left"
     }
-    return this;
 }
 
 function expressionToRpn(expression) {
-        let outputQueue = "";
-        let operatorStack = [];
-        let operators = {
-            "^": {
-                precedence: 4,
-                associativity: "Right"
-            },
-            "/": {
-                precedence: 3,
-                associativity: "Left"
-            },
-            "*": {
-                precedence: 3,
-                associativity: "Left"
-            },
-            "+": {
-                precedence: 2,
-                associativity: "Left"
-            },
-            "-": {
-                precedence: 2,
-                associativity: "Left"
+    let outputQueue = "";
+    let operatorStack = [];
+    expression = expression.replace(/\s+/g, "");
+    expression = expression.split(/([\+\-\*\/\^\(\)])/).filter(character => character)
+    expression.forEach(function(item){
+        let token = item;
+        if (token === (+token).toString()) {
+            outputQueue += token + " ";
+        } else if ("^*/+-".indexOf(token) !== -1) {
+            let o1 = token;
+            let o2 = operatorStack[operatorStack.length - 1];
+            while (operator(o1,o2)) {
+                outputQueue += operatorStack.pop() + ` `;
+                o2 = operatorStack[operatorStack.length - 1];
             }
-        }
-        expression = expression.replace(/\s+/g, "");
-        expression = expression.split(/([\+\-\*\/\^\(\)])/).clean();
-        for(let i = 0; i < expression.length; i++) {
-            let token = expression[i];
-            if(token == +token) {
-                outputQueue += token + " ";
-            } else if("^*/+-".indexOf(token) !== -1) {
-                let o1 = token;
-                let o2 = operatorStack[operatorStack.length - 1];
-                while("^*/+-".indexOf(o2) !== -1 && ((operators[o1].associativity === "Left" && operators[o1].precedence <= operators[o2].precedence) || (operators[o1].associativity === "Right" && operators[o1].precedence < operators[o2].precedence))) {
-                    outputQueue += operatorStack.pop() + " ";
-                    o2 = operatorStack[operatorStack.length - 1];
-                }
-                operatorStack.push(o1);
-            } else if(token === "(") {
-                operatorStack.push(token);
-            } else if(token === ")") {
-                while(operatorStack[operatorStack.length - 1] !== "(") {
-                    outputQueue += operatorStack.pop() + " ";
-                }
-                operatorStack.pop();
+            operatorStack.push(o1);
+        } else if (token === "(") {
+            operatorStack.push(token);
+        } else if (token === ")") {
+            while (operatorStack[operatorStack.length - 1] !== "(") {
+                outputQueue += operatorStack.pop() + ` `;
             }
+            operatorStack.pop();
         }
-        while(operatorStack.length > 0) {
-            outputQueue += operatorStack.pop() + " ";
-        }
-        return outputQueue.replace('^', '**');
+    });
+    while (operatorStack.length > 0) {
+        outputQueue += operatorStack.pop() + ` `;
     }
+    return outputQueue.replace('^', '**');
+}
+
+function operator(o1,o2) {
+   return "^*/+-".indexOf(o2) !== -1 && ((operators[o1].associativity === "Left" && operators[o1].precedence <= operators[o2].precedence) || (operators[o1].associativity === "Right" && operators[o1].precedence < operators[o2].precedence))
+}
 
 function calculateRpnExpression(str) {
-    s = []
-    str = str.split(' ')
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] == +str[i]) s.push(str[i])
-        else if (str[i] == '+') s.push(+s.splice(-2,1)[0] + +s.pop())
-        else if (str[i] == '-') s.push(+s.splice(-2,1)[0] - +s.pop())
-        else if (str[i] == '*') s.push(+s.splice(-2,1)[0] * +s.pop())
-        else if (str[i] == '/') s.push(+s.splice(-2,1)[0] / +s.pop())
-        else if (str[i] == '**') s.push(Math.pow(+s.splice(-2,1)[0], +s.pop()))
-    }
-    return s[0];
+    result = []
+    let expression = str.split(' ')
+    expression.forEach(function(item){
+        switch (item) {
+          case (+item).toString():
+            result.push(item);
+            break;
+          case '+':
+            result.push(+result.splice(-2,1)[0] + +result.pop());
+            break;
+          case '-':
+            result.push(+result.splice(-2,1)[0] - +result.pop());
+            break;
+          case '*':
+            result.push(+result.splice(-2,1)[0] * +result.pop());
+            break;
+          case '/':
+            result.push(+result.splice(-2,1)[0] / +result.pop());
+            break;
+          case '**':
+            result.push(Math.pow(+result.splice(-2,1)[0], +result.pop()));
+            break;
+        }
+    });
+    return result[0];
 }
 
 
