642ba52ead89b23e5350a2278272d246eaa32514
maximpristupa <g.maxim.pristupa@gmail.com>
2019-11-12 16:06:21 +0300

    sorting changed

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
+"use strict";
+
 const express = require('express')
 const bodyParser= require('body-parser')
 const multer = require('multer');
 
-const calc = require('../evaluate')
-const sort = require('../sorting')
-const rw = require('../rw')
+const calc = require('./evaluate')
+const sort = require('./sorting')
+const rw = require('./rw')
  
 const app = express();
 app.set("view engine", "hbs");
@@ -36,22 +38,22 @@ app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
   let objects = sort.csvToObjects(rw.readCsv(file.path))
   let csv = rw.objectsToCsv(objects) 
   rw.writeCsv(csv, file.path)
-  res.render("calc", {csv: csv, row_count: csv.length});
+  res.render("calc", {csv: csv, rowCount: csv.length});
   
 })
 
 app.post('/calculate', (req, res, next) => {
   let csv = req.body.cell
-  let row_count = req.body.row_count
-  let size = csv.length / +row_count;
+  let rowCount = req.body.rowCount
+  let size = csv.length / +rowCount;
   let subarray = [];
   for (let i = 0; i <Math.ceil(csv.length/size); i++){
     subarray[i] = csv.slice((i*size), (i*size) + size).join();
   }
 
   let objects = sort.csvToObjects(subarray)
-  let calculated_csv = rw.objectsToCsv(objects)
-  res.render("calc", {csv: calculated_csv, row_count: calculated_csv.length});
+  let calculatedCsv = rw.objectsToCsv(objects)
+  res.render("calc", {csv: calculatedCsv, rowCount: calculatedCsv.length});
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'));
---
+++ server/sorting.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const fs = require('fs');
+const calc = require('./evaluate')
+
+let calculatedСells = []
+function csvToObjects(csv) {
+	let objects = []	
+	let objectType = typeof csv
+	let lines
+	if (objectType == 'string') {lines = csv.split("\n");} else {lines = csv;}
+	lines.forEach((line, lineIndex) => {
+	let cells = line.split(",");
+		cells.forEach((cell, cellIndex) => {
+			if (!cell) {return;}
+	  	let links = cell.match(/[A-Z]\d/g)
+	  	let chr = String.fromCharCode(65 + cellIndex);
+	  	let obj = {
+	  		name: `${chr}${lineIndex+1}`,
+	  		value: cell.replace('=',''),
+	  		links: links
+	  	};
+	  	if (!links) { calculatedСells.push(obj); }
+	  	objects.push(obj)
+		})
+	});
+
+	while(calculatedСells.length < objects.length){
+		let readyForCalc = unsettledCells(objects).filter(i => i.links.filter(x => !arrayOfNames(calculatedСells).includes(x)).length == 0);
+		if (readyForCalc.length == 0) {removeLinks(unsettledCells(objects), "Error")}
+		readyForCalc.forEach((calcCell) => {
+			calculatedСells.push(calcCell);
+		});
+		readyForCalc.forEach((cell) => {
+			function replacer(match) {
+				let found = calculatedСells.find(function(element) { 
+	  			return element.name == match;
+				});
+				let value = '('+ found.value + ')'
+				return value
+			}
+			cell.value = cell.value.replace(/[A-Z]\d/g, replacer);
+			cell.links = null
+		});
+	}
+
+	calculatedСells.forEach((cell) => {
+		if (cell.value.match(/error/i) || cell.value === `${+cell.value}`) {return;}
+		cell.value = calc.evaluate(cell.value)
+	})
+	return objects
+}
+
+function unsettledCells(objects) {
+	return objects.filter(obj => obj.links)
+}
+
+function arrayOfNames(objects) {
+	return  objects.map(function(num) {
+	  return num.name;
+	});
+}
+
+function removeLinks(objects, message = 'Error') {
+	objects.forEach((cell) => {
+		cell.links = null
+		cell.value = message
+		calculatedСells.push(cell)
+	})
+}
+
+module.exports.csvToObjects = csvToObjects
