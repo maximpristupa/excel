8d0cabd954aaa975193e0210e77f7d2793bf7366
maximpristupa <g.maxim.pristupa@gmail.com>
2019-11-19 14:57:08 +0300

    refactoring

---
+++ server/views/script.js
@@ -0,0 +1,51 @@
+function upload(file) {
+  let formData = new FormData(document.forms.uploadFile);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/uploadfile");
+  xhr.send(formData);
+
+  xhr.onload = () => printHtml(displayPage(xhr.response));
+}
+
+function displayPage(response = null) {
+  if (!response) {return;}
+  let csv = JSON.parse(response)
+  let charCodeAt = 'A'.charCodeAt(0);
+  let rowNumber = 1;
+  let html = ''
+  html += '<form name="extable" class="extable">';
+  csv.forEach((row, rowIndex) => {
+      if (rowIndex === 0) {
+        let rowLetter = '';
+        for (let i = 0; i < row.length; i++) {
+          rowLetter += `<div class="letterRow">${String.fromCharCode(charCodeAt)}</div>`;
+          charCodeAt += 1;
+        }
+        html += rowLetter;
+      }
+      html += '<div class="row">';
+      html += `<div class="numberRow">${rowNumber}</div>`;
+      rowNumber += 1;
+      row.forEach((cell) => {
+          html += `<div class="cell"> <input class="cellInput" onkeydown = "if (event.keyCode == 13) calculate()" type="text" name="cell" value="${cell}"> </div>`;
+      });
+      html += '</div>';
+  });
+  html += `<input type="hidden" name="rowCount" value="${csv.length}">`;
+  html += '<input onclick="calculate()" value="Recalculate" type="button" id="recalcButton"/>';
+  html += '</form>';
+  return html;
+}
+
+function printHtml(html) {
+  document.getElementById('table').innerHTML = html;
+}
+
+function calculate() {
+  let formData = new FormData(document.forms.extable);
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/calculate");
+  xhr.send(formData);
+
+  xhr.onload = () => printHtml(displayPage(xhr.response));
+}
