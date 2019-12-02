f018b26d0c7e97eed133eaa054106833dc6b715a
maximpristupa <g.maxim.pristupa@gmail.com>
2019-12-03 01:08:39 +0300

    add buttons

--- server/server.js
+++ server/server.js
@@ -12,53 +12,66 @@ const rw = require('./rw');
 const app = express();
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }))
 
 app.use(express.static('views'));
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) {});
+
+app.get('/lastfiles', function(req, res) {
+  let jsonFile = fs.readFileSync('files.json');
+  let jsonName = JSON.parse(jsonFile);
+  res.send(jsonName);
 });
 
 let storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
+  destination: function(req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 })
 
 let upload = multer({
-    storage: storage
+  storage: storage
 })
 
 app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-    const file = req.file;
-    let objects = sort.csvToObjects(rw.readCsv(file.path));
-    let csv = rw.objectsToCsv(objects);
-    res.send(csv);
+  const file = req.file;
+  let objects = sort.csvToObjects(rw.readCsv(file.path));
+  let csv = rw.objectsToCsv(objects);
+  res.send(csv);
 })
 
 app.post('/calculate', upload.single('extable'), (req, res, next) => {
-    let csv = req.body.cell;
-    let rowCount = req.body.rowCount;
-    let size = csv.length / +rowCount;
-    let subarray = [];
-    for (let i = 0; i < Math.ceil(csv.length / size); i++) {
-        subarray[i] = csv.slice((i * size), (i * size) + size).join();
-    }
-    let objects = sort.csvToObjects(subarray);
-    let calculatedCsv = rw.objectsToCsv(objects);
-    if (req.body.download) {
-        fs.writeFile('newfile.csv', calculatedCsv, function (err) {
-          if (err) throw err;
-          console.log('File is created successfully.');
-        }); 
-    }
-    // if (req.body.download) {rw.writeCsv(calculatedCsv, 'uploads/')}
+  let csv = req.body.cell;
+  let rowCount = req.body.rowCount;
+  let size = csv.length / +rowCount;
+  let subarray = [];
+  for (let i = 0; i < Math.ceil(csv.length / size); i++) {
+    subarray[i] = csv.slice((i * size), (i * size) + size).join();
+  }
+  let objects = sort.csvToObjects(subarray);
+  let calculatedCsv = rw.objectsToCsv(objects);
+  let file = 'file' + Date.now() + '.csv';
+  if (req.body.filename) {
+    rw.writeCsv(calculatedCsv, file);
+    let jsonFile = fs.readFileSync('files.json');
+    let jsonName = JSON.parse(jsonFile);
+    jsonName[file] = req.body.filename;
+    fs.writeFileSync('files.json', JSON.stringify(jsonName));
+  }
 
-    res.send(calculatedCsv);
+  res.send(calculatedCsv);
+})
+
+app.get('/openfile', (req, res, next) => {
+  let file = `${fs.readFileSync(req.query.filename)}`;
+  let objects = sort.csvToObjects(file);
+  let csv = rw.objectsToCsv(objects);
+  res.send(csv);
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'));
--- server/views/script.js
+++ server/views/script.js
@@ -2,21 +2,53 @@ upload('newfile');
 
 function upload(file) {
   if (file === 'newfile') {
-   let array = [['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','',''],['','','','','','','','','','','','','','','','','']];
-   let json = JSON.stringify(array);
-   printHtml(displayPage(json));
-   return;
+    const emptyCsvArray = [
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+    ];
+    let json = JSON.stringify(emptyCsvArray);
+    printHtml(displayPage(json));
+    return;
   }
   let formData = new FormData(document.forms.uploadFile);
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "/uploadfile");
   xhr.send(formData);
-
   xhr.onload = () => printHtml(displayPage(xhr.response));
 }
 
 function displayPage(response = null) {
-  if (!response) {return;}
+  if (!response) {
+    return;
+  }
   let csv = JSON.parse(response)
   let csvLength = csv[0].length;
   let charCodeAt = 'A'.charCodeAt(0);
@@ -34,13 +66,13 @@ function displayPage(response = null) {
 
   html += '<form name="extable" class="extable">';
   csv.forEach((row, rowIndex) => {
-      html += '<div class="row">';
-      html += `<div class="numberRow" id="${rowIndex}_number"><div class="number">${rowNumber}</div></div>`;
-      rowNumber += 1;
-      row.forEach((cell, cellIndex) => {
-          html += `<div class="cell"> <input id="${rowIndex}_${cellIndex}" onmouseover="backlight('${rowIndex}_${cellIndex}')" onmouseout="removeBacklight('${rowIndex}_${cellIndex}')" class="cellInput cell" onclick="openInput(this)" onkeydown = "if (event.keyCode == 13) calculate()" type="text" name="cell" value="${cell}"> </div>`;
-      });
-      html += '</div>';
+    html += '<div class="row">';
+    html += `<div class="numberRow" id="${rowIndex}_number"><div class="number">${rowNumber}</div></div>`;
+    rowNumber += 1;
+    row.forEach((cell, cellIndex) => {
+      html += `<div class="cell"> <input id="${rowIndex}_${cellIndex}" onmouseover="backlight('${rowIndex}_${cellIndex}')" onmouseout="removeBacklight('${rowIndex}_${cellIndex}')" class="cellInput cell" onclick="openInput(this)" onkeydown = "if (event.keyCode == 13) calculate()" type="text" name="cell" value="${cell}"> </div>`;
+    });
+    html += '</div>';
   });
   html += `<input type="hidden" name="rowCount" value="${csv.length}">`;
   html += '</form>';
@@ -51,10 +83,10 @@ function printHtml(html) {
   document.getElementById('table').innerHTML = html;
 }
 
-function calculate(arg) {
+function calculate(filename) {
   let formData = new FormData(document.forms.extable);
-  if (arg === 'download') {
-    formData.append("download", "true");
+  if (filename) {
+    formData.append("filename", filename);
   }
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "/calculate");
@@ -65,6 +97,7 @@ function calculate(arg) {
 
 // input row
 let input = document.getElementsByClassName('cellInputForm')[0];
+
 function openInput(cell) {
   input.value = cell.value;
   input.setAttribute('cellId', cell.id);
@@ -81,26 +114,22 @@ function changeInput(cell) {
   };
 }
 
-// function inputHighlightTheMentionedCell() {
-//   input.oninput = function() {
-//     highlightTheMentionedCell(input.value);
-//   }
-// }
-
 function highlightTheMentionedCell(value) {
-    let cells = document.getElementsByClassName('cell');
-    for (let i = 0; i < cells.length; i++) {
-          cells[i].classList.remove("highlightRed");
-        }
-    let links = value.match(/[A-Z]\d+/g);
-    if (!links) {return;}
-    for (let i = 0; i < links.length; i++) {
-      const letter = links[i].match(/[A-Z]/)[0];
-      const number = links[i].match(/(\d+)/)[0];
-      let id = `${number - 1}_${letter.charCodeAt(0) - 65}`;
-      let linkCell = document.getElementById(id);
-      linkCell.classList.add("highlightRed");
-    }
+  let cells = document.getElementsByClassName('cell');
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].classList.remove("highlightRed");
+  }
+  let links = value.match(/[A-Z]\d+/g);
+  if (!links) {
+    return;
+  }
+  for (let i = 0; i < links.length; i++) {
+    const letter = links[i].match(/[A-Z]/)[0];
+    const number = links[i].match(/(\d+)/)[0];
+    let id = `${number - 1}_${letter.charCodeAt(0) - 65}`;
+    let linkCell = document.getElementById(id);
+    linkCell.classList.add("highlightRed");
+  }
 }
 
 function changeCell(cell) {
@@ -139,5 +168,61 @@ function removeBacklight(id) {
   let numberCell = document.getElementById(numberId + '_number');
   let letterCell = document.getElementById(letterId + '_letter');
   numberCell.style.backgroundColor = "#f3f3f5";
-  letterCell.style.backgroundColor = "#f3f3f5"; 
+  letterCell.style.backgroundColor = "#f3f3f5";
+}
+
+function download(argument) {
+  let modal = document.getElementsByClassName('filename')[0];
+  modal.classList.add("showfilename");
+  let container = document.getElementsByClassName('container')[0];
+  container.classList.add("cover");
+}
+
+function showModal(argument) {
+  let modal = document.getElementsByClassName('modal')[0];
+  modal.classList.add("showmodal");
+  let container = document.getElementsByClassName('container')[0];
+  container.classList.add("cover");
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "/lastfiles");
+  xhr.send();
+  xhr.onload = () => printFiles(xhr.response);
+}
+
+function writeFile(argument) {
+  let fileName = document.getElementsByClassName('inputfilename')[0].value
+  calculate(fileName)
+  let modal = document.getElementsByClassName('filename')[0];
+  modal.classList.remove("showfilename");
+}
+
+function printFiles(json) {
+  let modal = document.getElementsByClassName('modal')[0];
+  html = '';
+  html += '<p>Choose File</p>';
+  let records = JSON.parse(json);
+  for (let k in records) {
+    html += `<div class="modalfilename" onclick="uploadLatestFile('${k}')">${records[k]}</div>`
+  }
+  modal.innerHTML = html;
+}
+
+function uploadLatestFile(filename) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "/openfile" + `?filename=${filename}`);
+  xhr.send(filename);
+  xhr.onload = () => printHtml(displayPage(xhr.response));
+  let modal = document.getElementsByClassName('modal')[0];
+  modal.classList.remove("showmodal");
+  let container = document.getElementsByClassName('container')[0];
+  container.classList.remove("cover");
+}
+
+function closeCover(argument) {
+  let container = document.getElementsByClassName('container')[0];
+  container.classList.remove("cover");
+  let modal = document.getElementsByClassName('modal')[0];
+  modal.classList.remove("showmodal");  
+  let input = document.getElementsByClassName('filename')[0];
+  input.classList.remove("showfilename");
 }
