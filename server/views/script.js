0e5b4925b00674c920b473c937119dd219c43612
maximpristupa <g.maxim.pristupa@gmail.com>
2019-12-03 14:41:04 +0300

    style

--- server/views/script.js
+++ server/views/script.js
@@ -70,7 +70,7 @@ function displayPage(response = null) {
     html += `<div class="numberRow" id="${rowIndex}_number"><div class="number">${rowNumber}</div></div>`;
     rowNumber += 1;
     row.forEach((cell, cellIndex) => {
-      html += `<div class="cell"> <input id="${rowIndex}_${cellIndex}" onmouseover="backlight('${rowIndex}_${cellIndex}')" onmouseout="removeBacklight('${rowIndex}_${cellIndex}')" class="cellInput cell" onclick="openInput(this)" onkeydown = "if (event.keyCode == 13) calculate()" type="text" name="cell" value="${cell}"> </div>`;
+      html += `<div class="cell"> <input id="${rowIndex}_${cellIndex}" onmouseover="backlight('${rowIndex}_${cellIndex}', '#cacaca')" onmouseout="backlight('${rowIndex}_${cellIndex}', '#f3f3f5')" class="cellInput cell" onclick="openInput(this)" onkeydown = "if (event.keyCode == 13) calculate()" type="text" name="cell" value="${cell}"> </div>`;
     });
     html += '</div>';
   });
@@ -83,28 +83,25 @@ function printHtml(html) {
   document.getElementById('table').innerHTML = html;
 }
 
-function calculate(filename) {
-  let formData = new FormData(document.forms.extable);
-  if (filename) {
-    formData.append("filename", filename);
+  function calculate(filename) {
+    let formData = new FormData(document.forms.extable);
+    if (filename) {
+      formData.append("filename", filename);
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/calculate");
+    xhr.send(formData);
+
+    xhr.onload = () => printHtml(displayPage(xhr.response));
   }
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/calculate");
-  xhr.send(formData);
 
-  xhr.onload = () => printHtml(displayPage(xhr.response));
-}
-
-// input row
-let input = document.getElementsByClassName('cellInputForm')[0];
-
-function openInput(cell) {
+  let input = document.getElementsByClassName('cellInputForm')[0];
+  function openInput(cell) {
   input.value = cell.value;
   input.setAttribute('cellId', cell.id);
   changeInput(cell);
   changeCell(cell);
 }
-
 
 function changeInput(cell) {
   cell.oninput = function() {
@@ -153,36 +150,35 @@ function writeInput(argument) {
   input.value = cell.value;
 }
 
-function backlight(id) {
+function backlight(id, color) {
   let numberId = id.match(/(\d+)_/)[1];
   let letterId = id.match(/_(\d+)/)[1];
   let numberCell = document.getElementById(numberId + '_number');
   let letterCell = document.getElementById(letterId + '_letter');
-  numberCell.style.backgroundColor = "#cacaca";
-  letterCell.style.backgroundColor = "#cacaca";
+  numberCell.style.backgroundColor = color;
+  letterCell.style.backgroundColor = color;
 }
 
-function removeBacklight(id) {
-  let numberId = id.match(/(\d+)_/)[1];
-  let letterId = id.match(/_(\d+)/)[1];
-  let numberCell = document.getElementById(numberId + '_number');
-  let letterCell = document.getElementById(letterId + '_letter');
-  numberCell.style.backgroundColor = "#f3f3f5";
-  letterCell.style.backgroundColor = "#f3f3f5";
+function addClass(element, newClass) {
+  let modal = document.getElementsByClassName(element)[0];
+  modal.classList.add(newClass);
+}
+
+function removeClass(element, newClass) {
+  let modal = document.getElementsByClassName(element)[0];
+  modal.classList.remove(newClass);
 }
 
 function download(argument) {
-  let modal = document.getElementsByClassName('filename')[0];
-  modal.classList.add("showfilename");
-  let container = document.getElementsByClassName('container')[0];
-  container.classList.add("cover");
+  let input = document.getElementsByClassName('inputfilename')[0];
+  input.value = '';
+  addClass('filename', 'showfilename');
+  addClass('container', 'cover');
 }
 
 function showModal(argument) {
-  let modal = document.getElementsByClassName('modal')[0];
-  modal.classList.add("showmodal");
-  let container = document.getElementsByClassName('container')[0];
-  container.classList.add("cover");
+  addClass('modal', 'showmodal');
+  addClass('container', 'cover');
   let xhr = new XMLHttpRequest();
   xhr.open("GET", "/lastfiles");
   xhr.send();
@@ -190,16 +186,21 @@ function showModal(argument) {
 }
 
 function writeFile(argument) {
-  let fileName = document.getElementsByClassName('inputfilename')[0].value
-  calculate(fileName)
-  let modal = document.getElementsByClassName('filename')[0];
-  modal.classList.remove("showfilename");
+  let fileName = document.getElementsByClassName('inputfilename')[0].value;
+  calculate(fileName);
+  removeClass('filename', 'showfilename');
+  removeClass('container', 'cover');
 }
 
 function printFiles(json) {
   let modal = document.getElementsByClassName('modal')[0];
   html = '';
-  html += '<p>Choose File</p>';
+  if (json === '{}') {
+    html += '<p class="chooseFile">No files to open</p>';  
+    modal.innerHTML = html;
+    return;
+  }
+  html += '<p class="chooseFile">Choose File</p>';
   let records = JSON.parse(json);
   for (let k in records) {
     html += `<div class="modalfilename" onclick="uploadLatestFile('${k}')">${records[k]}</div>`
@@ -212,17 +213,12 @@ function uploadLatestFile(filename) {
   xhr.open("GET", "/openfile" + `?filename=${filename}`);
   xhr.send(filename);
   xhr.onload = () => printHtml(displayPage(xhr.response));
-  let modal = document.getElementsByClassName('modal')[0];
-  modal.classList.remove("showmodal");
-  let container = document.getElementsByClassName('container')[0];
-  container.classList.remove("cover");
+  removeClass('modal', 'showmodal');
+  removeClass('container', 'cover');
 }
 
 function closeCover(argument) {
-  let container = document.getElementsByClassName('container')[0];
-  container.classList.remove("cover");
-  let modal = document.getElementsByClassName('modal')[0];
-  modal.classList.remove("showmodal");  
-  let input = document.getElementsByClassName('filename')[0];
-  input.classList.remove("showfilename");
+  removeClass('container', 'cover');
+  removeClass('modal', 'showmodal');
+  removeClass('filename', 'showfilename');
 }
